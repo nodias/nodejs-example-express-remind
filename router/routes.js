85ed970ef864f48f7778c598caea01cdd7e08d2a
SeongSu Kang <seongsukang@SeongSuui-MacBookPro.local>
2019-10-20 14:03:27 +0900

    ejs

--- router/routes.js
+++ router/routes.js
@@ -1,7 +1,10 @@
 module.exports = function(app)
 {
     app.get('/', function(req, res){
-        res.render('index.html')
+        res.render('index', {
+            title: "MY HOMEPAGE",
+            length: 5
+        })
     });
     app.get('/about', function(req, res){
         res.render('about.html')
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
-const router = require('./router/routes')(app);
+const session = require('express-session');
+const fs = require('fs')
 
-app.set('views', __dirname + '/api/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-app.use(express.static('public'));
 
 app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Session middleware
+app.use(session({
+    secret: 'mysecret', //쿠키 변조 방지용
+    resave: false, //세션을 언제나 저장할 지 정하는 값
+    saveUninitialized: true // 새로 생겼지만 변경되지 않은 세션
+}));
+
+let router = require('./router/routes')(app, fs);
